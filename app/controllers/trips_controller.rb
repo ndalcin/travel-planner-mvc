abf99e5fb3d11827aa2f4aebc3f27a60c2d07211
ndalcin <dalcin.nicole@gmail.com>
2017-10-24 20:35:20 -0400

    edit form fixed

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -41,6 +41,7 @@ class TripsController < ApplicationController
         if @trip.user_id == current_user.id
           erb :"/trips/edit"
         else
+          flash[:notice] = "This is not your trip to edit!"
           redirect '/trips'
         end
       else
@@ -51,10 +52,33 @@ class TripsController < ApplicationController
     get "/trips/:id" do
       if logged_in?
         @trip = Trip.find_by_id(params[:id])
+        if !@trip
+          flash[:notice] = "This does not exist!"
+          redirect '/home'
+        end
         erb :"/trips/show"
       else
+        flash[:notice] = "Please login to view trips"
         redirect '/login'
       end
+    end
+
+    post "/trips/:id" do
+      @trip = Trip.find_by_id(params[:id])
+      @trip.update(params[:trip])
+      if params[:flight][:departure]
+        @trip.flight_id = params[:flight][:departure]
+      end
+      if !params[:departure_city].empty?
+        flight = Flight.find_by(departure_city: params[:departure_city])
+        if flight
+          @trip.flight = flight
+        else
+          @trip.flight = Flight.create(departure_city: params[:departure_city])
+        end
+      end
+        @trip.save
+        redirect "/trips/#{@trip.id}"
     end
 
     delete '/trips/:id/delete' do #delete action
@@ -62,6 +86,7 @@ class TripsController < ApplicationController
         @trip = Trip.find_by_id(params[:id])
         if @trip.user_id == current_user.id
           @trip.delete
+          flash[:notice] = "Your trip was deleted successfully!"
           redirect to "/home"
         else
           redirect '/trips'
@@ -70,8 +95,6 @@ class TripsController < ApplicationController
         redirect to '/login'
       end
     end
-
-
 
 
 end
